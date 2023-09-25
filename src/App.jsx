f92799f9544beb2fdf19b2855cd618162e331fa4
AGN <itzme907@gmail.com>
2023-09-25 17:32:01 +0300

    building the main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
 import { useState } from 'react'
 import './styles/App.css'
-function App() {
+import PersonalInfo from './components/PersonalInfo'
+import EducationInfo from './components/EducationInfo'
+import CompanyInfo from './components/CompanyInfo'
+import Preview from './components/Preview'
 
+function App() {
+  const [info, setInfo] = useState({
+    firstName: 'John',
+    lastName: 'Doe',
+    address: 'New York, USA',
+    email: 'example@example.com',
+    phoneNumber: '555-555-555',
+    schoolName: 'Harvard University',
+    degree: 'Bachelor of Computer Science',
+    graduationDate: 'May 2018',
+    companyName: 'Lavenar',
+    position: 'Full Stack Developer',
+    from: 'July 2018',
+    to: 'June 2023',
+    mainResponsibilities: 'Responsible for developing new user-facing features, determining the structure and design of web pages, building reusable codes, optimizing page loading times, and using a variety of markup languages to create web pages.',
+  })
+
+  function handleChange(newValue, label) {
+    console.log(label)
+    const newObj = { ...info }
+    let newValueCopy = newValue
+
+    newObj[label] = newValueCopy
+    setInfo(newObj)
+  }
 
 
   return (
     <>
+      <main>
 
+        <div>
+          <PersonalInfo objValue={info} handleChange={handleChange} />
+          <EducationInfo objValue={info} handleChange={handleChange} />
+          <CompanyInfo objValue={info} handleChange={handleChange} />
+
+
+        </div>
+        <Preview data={info} />
+
+      </main>
     </>
   )
 }
